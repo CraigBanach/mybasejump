5897c10acbb735eb58fdb6190ee23918fad7de66
Craig Banach <craigsb93@gmail.com>
2016-03-28 15:41:10 +0000

    Finished project

--- server.js
+++ server.js
@@ -1,33 +1,80 @@
 'use strict';
 
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
-
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
+var strftime = require("strftime");
 
-mongoose.connect(process.env.MONGO_URI);
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
+app.get("/:query", function(req, res) {
+    var output = parseQuery(req.params.query);
+    res.end(JSON.stringify(output));
+    console.log(output);
+})
 
 var port = process.env.PORT || 8080;
-app.listen(port,  function () {
+var server = app.listen(port, function () {
 	console.log('Node.js listening on port ' + port + '...');
+	console.log("Listening at address " + server.address().address)
 });
+
+function parseQuery(query) {
+	if (!isNaN(query)) {
+		//convert to readable time
+		var unix = parseInt(query);
+		var readable = strftime("%B %e, %Y", new Date(unix));
+	} else {
+		//Check if it is a readable date
+		
+		try {
+			var readable = checkForReadable(query);
+		} catch (err) {
+			console.log(err);
+			return { 
+				"unix": null, 
+				"natural": null
+				
+			};
+		}
+		try {
+			var unix = new Date(readable).getTime();
+		} catch (err) {
+			console.log(err);
+			return {
+				"unix": null,
+				"natural": null
+			}
+		}
+	}
+	return {
+			"unix": unix,
+			"natural": readable
+		};
+}
+
+function checkForReadable(query) {
+	
+	var regex = new RegExp(/[\ ,\-\\\/]/g);
+	var queryArray = query.split(regex);
+	var arrayLength = queryArray.length;
+	
+	while (arrayLength > 3) {
+		
+		var smallestPosition = 0;
+		var smallestLength = queryArray[0].length;
+		
+		for (var i = 1; i < arrayLength; i++) {
+			if (queryArray[i].length < smallestLength) {
+				smallestPosition = i;
+				smallestLength = queryArray[i].length;
+			}
+		}
+		//console.log(queryArray[3].length + "   " +  smallestPosition);
+		queryArray.splice(smallestPosition, 1);
+		arrayLength--;
+	}
+	if (arrayLength === 3) {
+		return queryArray[0] + " " + queryArray[1] + ", " + queryArray[2];
+	} else {
+		throw "Query String contains less than 3 data points."
+	}
+}
